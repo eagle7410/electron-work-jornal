59f74f9e83e47f48b69811017acfbaf99e0e4a15
eagle7410 <verycooleagle@gmail.com>
2017-08-18 19:30:44 +0300

    Add icons.

--- app/src/componets/Settings/Settings.js
+++ app/src/componets/Settings/Settings.js
@@ -2,10 +2,7 @@ import React from 'react';
 import NavMenu from '../NavMenu/NavMenu';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import Paper from 'material-ui/Paper';
-import IconSDF from 'material-ui/svg-icons/editor/insert-drive-file'
 import IconDropBox from 'material-ui/svg-icons/action/cached'
-import SdfTools from './SdfTools'
-import SdfPanel from './SdfPanel'
 import DropBox from './DropBox'
 import {tabSettings} from '../../const/Styles'
 
@@ -17,12 +14,6 @@ const Settings = () => (
 			<Tab label='DropBox' icon={<IconDropBox />}  style={tabSettings}>
 				<Paper zDepth={2}>
 					<DropBox/>
-				</Paper>
-			</Tab>
-			<Tab label='Load SDF' icon={<IconSDF />}  style={tabSettings}>
-				<Paper zDepth={2}>
-					<SdfTools/>
-					<SdfPanel/>
 				</Paper>
 			</Tab>
 		</Tabs>

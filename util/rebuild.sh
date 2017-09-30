e9fca3afdc82a25b7457a8049c43edd914ad5ed9
Igor <verycooleagle@gmail.com>
2017-09-30 14:57:32 +0300

    Start update drop-box module

--- util/rebuild.sh
+++ util/rebuild.sh
@@ -8,17 +8,17 @@ appName="electron-work-jornal"
 folderDesk = "Рабочий\ стол"
 folder="/home/igor/${folderDesk}/projects/${appName}/app_build"
 
-lin=$folder"/"$appName"-linux-x64"
-linZip=$lin".tar.gz"
-
-mac=$folder"/"$appName"-darwin-x64"
-macZip=$mac".tar.gz"
-
-win=$folder"/"$appName"-win32-ia32"
-winZip=$win".tar.gz"
-
-tar -czvf $linZip $lin
-tar -czvf $macZip $mac
-tar -czvf $winZip $win
+#lin=$folder"/"$appName"-linux-x64"
+#linZip=$lin".tar.gz"
+#
+#mac=$folder"/"$appName"-darwin-x64"
+#macZip=$mac".tar.gz"
+#
+#win=$folder"/"$appName"-win32-ia32"
+#winZip=$win".tar.gz"
+#
+#tar -czvf $linZip $lin
+#tar -czvf $macZip $mac
+#tar -czvf $winZip $win
 
 echo "finish build"
